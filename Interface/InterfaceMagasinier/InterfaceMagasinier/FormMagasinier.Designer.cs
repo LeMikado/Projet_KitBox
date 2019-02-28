aa1974b640facecf9b1a43e5d7df180a2f453dad
Michaël <micscarito@gmail.com>
2019-02-28 16:04:28 +0100

    Adding Interface Magasinier
    Added and began unfinished orders

---
+++ Interface/InterfaceMagasinier/InterfaceMagasinier/FormMagasinier.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace InterfaceMagasinier
+{
+    partial class FormMagasinier
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMagasinier));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.finishedOrdersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unfinishedOrdersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.unfinishedOrders1 = new InterfaceMagasinier.UnfinishedOrders();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.finishedOrdersToolStripMenuItem,
+            this.unfinishedOrdersToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(55, 22);
+            this.toolStripDropDownButton1.Text = "Orders";
+            // 
+            // finishedOrdersToolStripMenuItem
+            // 
+            this.finishedOrdersToolStripMenuItem.Name = "finishedOrdersToolStripMenuItem";
+            this.finishedOrdersToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.finishedOrdersToolStripMenuItem.Text = "Finished orders";
+            // 
+            // unfinishedOrdersToolStripMenuItem
+            // 
+            this.unfinishedOrdersToolStripMenuItem.Name = "unfinishedOrdersToolStripMenuItem";
+            this.unfinishedOrdersToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.unfinishedOrdersToolStripMenuItem.Text = "Unfinished orders";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(40, 22);
+            this.toolStripButton1.Text = "Stock";
+            // 
+            // unfinishedOrders1
+            // 
+            this.unfinishedOrders1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.unfinishedOrders1.Location = new System.Drawing.Point(21, 28);
+            this.unfinishedOrders1.Name = "unfinishedOrders1";
+            this.unfinishedOrders1.Size = new System.Drawing.Size(767, 449);
+            this.unfinishedOrders1.TabIndex = 1;
+            // 
+            // FormMagasinier
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.unfinishedOrders1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FormMagasinier";
+            this.Text = "KitBox - Magasinier";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem finishedOrdersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unfinishedOrdersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private UnfinishedOrders unfinishedOrders1;
+    }
+}
+
---
+++ Interface/InterfaceMagasinier/InterfaceMagasinier/UnfinishedOrders.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace InterfaceMagasinier
+{
+    partial class UnfinishedOrders
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.orderNumber = new System.Windows.Forms.Label();
+            this.cabinet = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.Label();
+            this.gsm = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchTextBox.Location = new System.Drawing.Point(728, 21);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Location = new System.Drawing.Point(675, 21);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(47, 13);
+            this.searchLabel.TabIndex = 1;
+            this.searchLabel.Text = "Search :";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Location = new System.Drawing.Point(589, 53);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(110, 43);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Confirm";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(706, 53);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(115, 43);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Controls.Add(this.name, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.gsm, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.price, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cancelButton, 6, 1);
+            this.tableLayoutPanel1.Controls.Add(this.closeButton, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.orderNumber, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cabinet, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 57);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(825, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // orderNumber
+            // 
+            this.orderNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.orderNumber.AutoSize = true;
+            this.orderNumber.Location = new System.Drawing.Point(4, 1);
+            this.orderNumber.Name = "orderNumber";
+            this.orderNumber.Size = new System.Drawing.Size(110, 48);
+            this.orderNumber.TabIndex = 4;
+            this.orderNumber.Text = "Order number";
+            this.orderNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cabinet
+            // 
+            this.cabinet.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cabinet.AutoSize = true;
+            this.cabinet.Location = new System.Drawing.Point(121, 1);
+            this.cabinet.Name = "cabinet";
+            this.cabinet.Size = new System.Drawing.Size(110, 48);
+            this.cabinet.TabIndex = 5;
+            this.cabinet.Text = "Cabinet";
+            this.cabinet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // price
+            // 
+            this.price.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.price.AutoSize = true;
+            this.price.Location = new System.Drawing.Point(238, 1);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(110, 48);
+            this.price.TabIndex = 6;
+            this.price.Text = "Price";
+            this.price.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gsm
+            // 
+            this.gsm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gsm.AutoSize = true;
+            this.gsm.Location = new System.Drawing.Point(355, 1);
+            this.gsm.Name = "gsm";
+            this.gsm.Size = new System.Drawing.Size(110, 48);
+            this.gsm.TabIndex = 7;
+            this.gsm.Text = "Phone number";
+            this.gsm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // name
+            // 
+            this.name.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(472, 1);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(110, 48);
+            this.name.TabIndex = 8;
+            this.name.Text = "Name";
+            this.name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UnfinishedOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "UnfinishedOrders";
+            this.Size = new System.Drawing.Size(834, 375);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label orderNumber;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label gsm;
+        private System.Windows.Forms.Label price;
+        private System.Windows.Forms.Label cabinet;
+    }
+}
